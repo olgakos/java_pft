38ee64a75a96dd7632a615b7532a9d272fa8f204
Olga <49266934+olgakos@users.noreply.github.com>
2020-05-19 17:17:06 +0300

    Изменена струкрура кода (Меню CODE --- Reformat)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 public class HelloWorld {
-    public static void main(String[] args) { System.out.println("Hello World!"); }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
