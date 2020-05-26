1379de1610eb117bd0e71918a2dd6503290bdb88
Olga <49266934+olgakos@users.noreply.github.com>
2020-05-26 18:21:04 +0300

    HW4  метод fillContactForm стал универсальным, в тест передает конкретное значение

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -19,15 +19,15 @@ public class ContactCreationTests {
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         //ниже  - это перенесенный сюда залогин
         wd.get("http://localhost/addressbook/");
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String username, String password) {
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
@@ -35,7 +35,7 @@ public class ContactCreationTests {
     public void testContactCreation() throws Exception {
         //раньше тут были строки про залогин.... теперь они выше
         GoToContactPage();
-        fillContactForm();
+        fillContactForm("Daniel", "Jacob", "Radcliffe", "Harry", "WB", "London", "111-11-11", "222-22-22", "333-33-33", "daniel@wb.uk");
         submitContactCreation();
         returnToHomePage();
         ClickToLogout();
@@ -53,38 +53,38 @@ public class ContactCreationTests {
         wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
     }
 
-    private void fillContactForm() {
+    private void fillContactForm(String FirstName, String MiddleName, String LastName, String Nickname, String Company, String Address, String TelHome, String TelMobile, String TelWork, String Email1) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Daniel");
+        wd.findElement(By.name("firstname")).sendKeys(FirstName);
         wd.findElement(By.name("middlename")).click();
         wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys("Jacob");
+        wd.findElement(By.name("middlename")).sendKeys(MiddleName);
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Radcliffe");
+        wd.findElement(By.name("lastname")).sendKeys(LastName);
         wd.findElement(By.name("nickname")).click();
         wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys("Harry");
+        wd.findElement(By.name("nickname")).sendKeys(Nickname);
         wd.findElement(By.name("company")).click();
         wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys("WB");
+        wd.findElement(By.name("company")).sendKeys(Company);
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("London");
+        wd.findElement(By.name("address")).sendKeys(Address);
         wd.findElement(By.name("theform")).click();
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys("111-11-11");
+        wd.findElement(By.name("home")).sendKeys(TelHome);
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("222-22-22");
+        wd.findElement(By.name("mobile")).sendKeys(TelMobile);
         wd.findElement(By.name("work")).click();
         wd.findElement(By.name("work")).clear();
-        wd.findElement(By.name("work")).sendKeys("333-33-33");
+        wd.findElement(By.name("work")).sendKeys(TelWork);
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("daniel@wb.uk");
+        wd.findElement(By.name("email")).sendKeys(Email1);
     }
 
     private void GoToContactPage() {
