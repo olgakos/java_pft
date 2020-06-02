8a53265a8c9c89178aa179a9459bc9232a778878
Olga <49266934+olgakos@users.noreply.github.com>
2020-06-02 13:48:00 +0300

    ДЗ Задание №7 запускать тесты в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplocationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplocationManager.java
@@ -29,11 +29,11 @@ public class ApplocationManager {
 
     public void init() {
         //String browser = BrowserType.FIREFOX;
-        if  (browser == BrowserType.FIREFOX) {
+        if  (browser.equals(FIREFOX)) {
              wd = new FirefoxDriver();
-        }    else if (browser == BrowserType.CHROME) {
+        }    else if (browser.equals(BrowserType.CHROME)) {
              wd = new ChromeDriver();
-        }    else if (browser == BrowserType.IE) {
+        }    else if (browser.equals(BrowserType.IE)) {
              wd = new InternetExplorerDriver();
         }
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
