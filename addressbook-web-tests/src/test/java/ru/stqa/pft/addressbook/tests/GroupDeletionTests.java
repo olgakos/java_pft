933168ef2ccf401d041bcbba6ed8b4be20ecdcac
Olga <49266934+olgakos@users.noreply.github.com>
2020-06-23 13:30:12 +0300

    Un5.6 работают все тесты с группами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.util.List;
 import java.util.Set;
+
+//это статические импорты из 5.6 связаны с тем что ниже
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -21,15 +29,16 @@ public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
-    Set<GroupData> before = app.group().all();
+    Groups before = app.group().all();
     GroupData deleteGroup = before.iterator().next();
     app.group().delete(deleteGroup);
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(), before.size() - 1);
+    Groups after = app.group().all();
+    assertEquals(after.size(), before.size() - 1);
 
     //это из модуля 4.6: спавнение списков groupdata
-    before.remove(deleteGroup);
-    Assert.assertEquals(before, after);
+   // before.remove(deleteGroup); ушло в 5.6
+    assertThat(after, equalTo(before.without(deleteGroup)));
+    //Assert.assertEquals(before, after); ушло в 5.6
 
   }
 
