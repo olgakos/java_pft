17f9c5f1bfacc7b4c8320675bfcd3b90c67aeb84
Olga <49266934+olgakos@users.noreply.github.com>
2020-07-02 22:44:39 +0300

    HW 9_2  (G+C: all ok Unit4.3)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,13 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTests extends TestBase {
 
 
-  @Test (enabled = false)
+  @Test
   public void testContactDeletion() throws Exception {
+    //1 строка ниже это из 4.3, int BEFORE
+    int before = app.getContactHelper().getContactCount();
     if (!app.getContactHelper().isThereAContact()){
       app.getContactHelper().createContact(new ContactData("Daniel", "Jacob", "Radcliffe", "Harry", "WB", "London", "111-11-11", "222-22-22", "333-33-33", "daniel@wb.uk", "test1"), true);
     }
@@ -15,6 +18,9 @@ public class ContactDeletionTests extends TestBase {
     app.getContactHelper().deleteSelectedContact();
     //строка ниже это искусственно вставленная команда закрытия всплывающего окна при удалении контакта
     app.getContactHelper().closeAlertfromDelete();
+//2строкb ниже это из 4.3, int AFTER
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after,before - 1);
   }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,13 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactModificationTests extends TestBase {
 
-    @Test (enabled = false)
+    @Test
+    //@Test (enabled = false)
     public void testContactModification() {
-        //int before = app.getGroupHelper().getGroupCount();
+        //1 строка ниже это из 4.3, int BEFORE
+        int before = app.getContactHelper().getContactCount();
         app.goTo().gotoHomePage();
         if (!app.getContactHelper().isThereAContact()){
             app.getContactHelper().createContact(new ContactData("Daniel", "Jacob", "Radcliffe", "Harry", "WB", "London", "111-11-11", "222-22-22", "333-33-33", "daniel@wb.uk", "test1"), true);
@@ -17,11 +20,10 @@ public class ContactModificationTests extends TestBase {
         app.getContactHelper().fillContactForm(new ContactData("Daniel1", "Jacob1", "Radcliffe1", "Harry", "WB", "London", "111-11-11", "222-22-22", "333-33-33", "daniel@wb.uk", null), false);
         app.getContactHelper().submitContactModification();
         app.getContactHelper().returnToHomePage();
-        app.getContactHelper().ClickToLogout();
-        //int after = app.getGroupHelper().getGroupCount();
-        //Assert.assertEquals(after, before);
-
-
-
+        //2строки ниже это из 4.3, int AFTER
+        int after = app.getContactHelper().getContactCount();
+        Assert.assertEquals(after, before);
+         //Важно!! строка ниже (логаут) убрана в ходе выпослнения задания №9. Не знаю, куда приткнуть.
+        //app.getContactHelper().ClickToLogout();
     }
 }
